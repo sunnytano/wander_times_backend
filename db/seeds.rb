538eb671ca2303a70d7b05bd45b21dc57ef73ed1
Susan Tan <ssusantan9@gmail.com>
2019-08-22 15:51:27 -0400

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,15 @@
 # require 'activerecord-reset-pk-sequence'
 
 # Article.destroy_all
-# Article.reset_pk_sequence
-
-# puts "seed complete 😻"
 
 require 'httparty'
 require 'json'
 
 # url = "https://newsapi.org/v2/everything?q=Apple&from=2019-08-13&sortBy=popularity&apiKey=#{key}"
 # key = "f0a7beb8be6040cfadf7471e6a6676b4"
-
-
         request = HTTParty.get("https://newsapi.org/v2/everything?q=Tech&from=2019-08-13&sortBy=popularity&apiKey=f0a7beb8be6040cfadf7471e6a6676b4")
         @request_hash=JSON.parse(request.body)
             @request_hash["articles"].each do |article|
             Article.create(title: article["title"], author: article["author"], overview: article["description"], url: article["url"], image: article["urlToImage"])
             puts article["title"]
-
-
 end 
